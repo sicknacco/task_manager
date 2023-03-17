eddd6738c7ec557763313153d42bd269fd8a2503
Nick Sacco <nicksacco10@gmail.com>
2023-03-17 10:46:36 -0700

    Complete writing to database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,13 +7,14 @@ class TasksController < ApplicationController
   end
 
   def create
+    binding.pry
     task = Task.new({
       title: params[:task][:title],
       description: params[:task][:description]
     })
-
+    binding.pry
     task.save
-
+    binding.pry
     redirect_to '/tasks'
   end
 end
