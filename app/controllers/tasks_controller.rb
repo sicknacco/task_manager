7653bf546051e794a6789713366c7ca6c69be8a9
Nick Sacco <nicksacco10@gmail.com>
2023-03-17 12:06:04 -0700

    Update task resource

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,7 +16,20 @@ class TasksController < ApplicationController
   end
 
   def show
-    binding.pry
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+    task.save
+    redirect_to "/tasks/#{task.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: "tasks#show"
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
 end
