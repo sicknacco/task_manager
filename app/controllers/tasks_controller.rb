5790738e8457d13eb915594a45e7dd621f288158
Nick Sacco <nicksacco10@gmail.com>
2023-03-17 10:55:32 -0700

    Complete Reading from db

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,20 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+    @tasks = Task.all
   end
 
   def new
   end
 
   def create
-    binding.pry
+    
     task = Task.new({
       title: params[:task][:title],
       description: params[:task][:description]
     })
-    binding.pry
+    
     task.save
-    binding.pry
+    
     redirect_to '/tasks'
   end
 end
