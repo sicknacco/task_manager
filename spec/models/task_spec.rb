2921fe2736c1d792e11eafe2e6d6427712ef644c
Nick Sacco <nicksacco10@gmail.com>
2023-03-20 13:38:19 -0700

    wip

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,8 +11,38 @@ RSpec.describe Task, type: :model do
 
       it 'returns true when the description is laundry' do
         task = Task.create!(title: 'Clean my clothes', description: 'laundry')
-    
-        expect(task.laundry?).to be(true)
+        
+        expect(task.laundry?).to be(true) 
+      end
+      
+      it 'returns false when neither the description or title is laundry' do
+        task = Task.create!(title: 'Do Dishes', description: 'dishes')
+        
+        expect(task.laundry?).to be(false)
+      end
+      
+      it 'returns true when the title contains the word laundry' do
+        task = Task.create!(title: 'Do the laundry', description: 'laundry')
+        
+        expect(task.contains?('laundry')).to be(true)
+      end
+      
+      xit 'is case insensitive when checking if the title contains the word laundry' do
+        task = Task.create!(title: 'Do the Laundry', description: 'laundry')
+        
+        expect(task.insensitive?('Laundry')).to be(true)
+      end
+      
+      xit 'returns true when the description contains the word laundry.' do
+        task = Task.create!(title: 'Do the Laundry', description: 'laundry')
+        
+        expect(task.description_contains?('laundry')).to be(true)
+      end
+      
+      xit 'is case insensitive when checking if the description contains the word laundry' do
+        task = Task.create!(title: 'Do the Laundry', description: 'Laundry')
+        
+        expect(task.description_contains?('Laundry')).to be(true)
       end
     end
   end
