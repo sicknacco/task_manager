dc774b246c10862ce00e69d787e842674ec21b69
Nick Sacco <nicksacco10@gmail.com>
2023-03-20 12:50:25 -0700

    wip

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
-  
+  def laundry?
+    if title == 'laundry'
+      return true
+    else
+      return false
+    end
+  end
 end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe 'instance methods' do
+    describe '#laundry?' do
+      it 'returns true when the title is laundry' do
+        task = Task.create!(title: 'laundry', description: 'clean clothes')
+
+        expect(task.laundry?).to be(true)
+      end
+    end
+  end
+end
