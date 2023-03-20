78d96a017f6a0a3cefd49a8aef12db77ef10a4eb
Nick Sacco <nicksacco10@gmail.com>
2023-03-20 12:57:15 -0700

    wip

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   def laundry?
     if title == 'laundry'
       return true
+    elsif description == 'laundry'
+      return true
     else
       return false
     end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Task, type: :model do
 
         expect(task.laundry?).to be(true)
       end
+
+      it 'returns true when the description is laundry' do
+        task = Task.create!(title: 'Clean my clothes', description: 'laundry')
+    
+        expect(task.laundry?).to be(true)
+      end
     end
   end
 end
